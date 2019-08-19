a0fe5d6dc547fbb76594b83f5565eb0a6f773805
lcgfly <48122100+lcgfly@users.noreply.github.com>
2019-08-19 15:48:57 +0800

    Update and rename 希尔排序.html to 希尔排序.js

--- 排序/希尔排序.js
+++ 排序/希尔排序.js
@@ -1,4 +1,4 @@
-<script>
+
 
 function shellSort(arr) {
     //第一层循环算出步长
@@ -26,4 +26,4 @@ function shellSort(arr) {
 let arr = [2,5,10,7,10,32,90,9,11,1,0,10];
 console.log(shellSort(arr));
 //[0, 1, 2, 5, 7, 9, 10, 10, 10, 11, 32, 90]
-</script>
+
