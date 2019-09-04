da3edbf10fa9f3952d2fe6019e1e03d015077b5a
lcgfly <48122100+lcgfly@users.noreply.github.com>
2019-09-04 16:24:14 +0800

    Update 实现Array flat.js

--- js/实现Array flat.js
+++ js/实现Array flat.js
@@ -1,1 +1,27 @@
+Array.prototype.diy = function (num = 1) {
+  
+          if (!Number(num) || num < 0) return this;
+  
+          let arr = [];
+  
+          this.forEach((item) => {
+            
+               if (Array.isArray(item)) {
+                 
+                    arr = arr.concat(item.diy(--num));
+                 
+               } else {
+                 
+                    arr.push(item);
+               }
+          })
+  
+          return arr;
+     }
 
+
+
+  //一行代码版
+     Array.prototype.diy = function () {
+          this.toString().split(',').map(item => +item);
+     }
