700bba68079b620d15646b95f6d1503eb5f75ff8
lcgfly <48122100+lcgfly@users.noreply.github.com>
2019-09-21 11:43:20 +0800

    Create call实现原理.js

---
+++ js/call实现原理.js
@@ -0,0 +1,17 @@
+//call方法实现原理
+        Function.prototype.myCall=function(context){
+            context = context?Object(context):window;  //null或undefined则指向window
+            context.fn=this;
+            var arr = [...arguments].slice(1);
+            if(arr.length>0){
+                return context.fn(...arr);
+            }
+        }
+
+        function demo(n1,n2){
+            console.log(this.name);
+            console.log(n1,n2);
+        }
+        var a={};
+        a.name='lyj';
+        demo.myCall(a,1,2);  //lyj 1 2
